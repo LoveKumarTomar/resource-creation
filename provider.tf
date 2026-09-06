15e4841e9f352f605a565e99b28dea1afbc276bc
LoveKumarTomar <lovetomar1289@gmail.com>
2026-09-06 16:32:41 +0530

    Update Terraform provider configuration

--- provider.tf
+++ provider.tf
@@ -1,13 +1,25 @@
-terraform {
+# terraform {
 
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "=4.1.0"
+#     }  
+#   }
+
+#    
+# }
+
+
+terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=4.1.0"
-    }  
+      version = "=5.0.0"
+    }
   }
 
-#    backend "azurerm" {
+# backend "azurerm" {
 #    resource_group_name   = "dev_res_grp"
 #     tenant_id            = "0d45acce-796c-4270-aa8a-b8340478dc5c"  
 #     storage_account_name = "devstorageaccountlove007"                              
@@ -21,5 +33,6 @@ terraform {
 provider "azurerm" {
   
   features {}
-  subscription_id = "ffe5b8ad-75fb-408a-bca0-fff28e876cb6"
+  # 
+  # subscription_id = "ffe5b8ad-75fb-408a-bca0-fff28e876cb6"
 }
